3e209054e3557c824489e39a058de47cffbfbb29
Philip Munksgaard <philip@munksgaard.me>
2022-04-24 10:48:38 +0200

    Update sway configuration

--- church/church.nix
+++ church/church.nix
@@ -119,12 +119,16 @@ in { config, pkgs, lib, ... }:
 
   programs.sway = {
     enable = true;
+    wrapperFeatures.gtk = true; # so that gtk works properly
     extraPackages = with pkgs; [
-      swaylock # lockscreen
+      swaylock
       swayidle
-      xwayland # for legacy apps
+      xwayland
+      wl-clipboard
       waybar # status bar
       mako # notification daemon
+      alacritty # Alacritty is the default terminal in the config
+      dmenu
       kanshi # autorandr
     ];
   };
@@ -138,33 +142,11 @@ in { config, pkgs, lib, ... }:
     };
   };
 
-  systemd.user.targets.sway-session = {
-    description = "Sway compositor session";
-    documentation = [ "man:systemd.special(7)" ];
-    bindsTo = [ "graphical-session.target" ];
-    wants = [ "graphical-session-pre.target" ];
-    after = [ "graphical-session-pre.target" ];
-  };
-
-  systemd.user.services.sway = {
-    description = "Sway - Wayland window manager";
-    documentation = [ "man:sway(5)" ];
-    bindsTo = [ "graphical-session.target" ];
-    wants = [ "graphical-session-pre.target" ];
-    after = [ "graphical-session-pre.target" ];
-    # We explicitly unset PATH here, as we want it to be set by
-    # systemctl --user import-environment in startsway
-    environment.PATH = lib.mkForce null;
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = ''
-        ${pkgs.dbus}/bin/dbus-run-session ${pkgs.sway}/bin/sway --debug
-      '';
-      Restart = "on-failure";
-      RestartSec = 1;
-      TimeoutStopSec = 10;
-    };
-  };
+  environment.loginShellInit = ''
+    if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
+      exec sway
+    fi
+  '';
 
   services.redshift = {
     enable = true;
@@ -335,21 +317,6 @@ in { config, pkgs, lib, ... }:
 
     # wally for keyboard config
     wally-cli
-
-    # Sway
-    (pkgs.writeTextFile {
-      name = "startsway";
-      destination = "/bin/startsway";
-      executable = true;
-      text = ''
-        #! ${pkgs.bash}/bin/bash
-
-        # first import environment variables from the login manager
-        systemctl --user import-environment
-        # then start the service
-        exec systemctl --user start sway.service
-      '';
-    })
 
     # For guix stuff
     guile
