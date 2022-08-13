2edce8b24cb59150a0f49c1fbf9fe735a8feec9c
Philip Munksgaard <philip@munksgaard.me>
2022-08-13 23:42:24 +0200

    rekey with server ssh

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,9 @@
 let
   munksgaard = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINlapwwXZyp/qTm1y9CA5WLVL33TAAznj5FkZW4/Ftvu";
+  server-root = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFv07cWCKk60J1+L/elzpxb/HHXN+FPBrsqQ1FXdkTxb";
 in
 {
-  "matrix-extra-conf.age".publicKeys = [ munksgaard ];
-  "gitea-mailer-password.age".publicKeys = [ munksgaard ];
-  "vaultwarden-environment.age".publicKeys = [ munksgaard ];
+  "matrix-extra-conf.age".publicKeys = [ munksgaard server-root ];
+  "gitea-mailer-password.age".publicKeys = [ munksgaard server-root ];
+  "vaultwarden-environment.age".publicKeys = [ munksgaard server-root ];
 }
