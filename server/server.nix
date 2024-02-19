9871594a4fd8bb4012ead575cca4480a7a8d99f6
Philip Munksgaard <philip@munksgaard.me>
2024-02-19 20:43:32 +0100

    Add podman to server

--- server/server.nix
+++ server/server.nix
@@ -376,4 +376,14 @@
   };
 
   services.cloud-init.enable = true;
+
+
+  virtualisation = {
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+    };
+  };
 }
