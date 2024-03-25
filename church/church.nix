8a8073dfc0bc39dc7840fe0caa6393c255753a23
Philip Munksgaard <philip@munksgaard.me>
2024-03-25 08:30:32 +0100

    Move wireplumber bluetooth config

--- church/church.nix
+++ church/church.nix
@@ -76,6 +76,17 @@ in {
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
+    wireplumber.configPackages = [
+      (pkgs.writeTextDir
+        "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
+          bluez_monitor.properties = {
+            ["bluez5.enable-sbc-xq"] = true,
+            ["bluez5.enable-msbc"] = true,
+            ["bluez5.enable-hw-volume"] = true,
+            ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+          }
+        '')
+    ];
   };
 
   # Bluetooth
@@ -151,14 +162,6 @@ in {
       # "sway/config".source = ./dotfiles/sway/config;
       # "xdg/waybar/config".source = ./dotfiles/waybar/config;
       # "xdg/waybar/style.css".source = ./dotfiles/waybar/style.css;
-      "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-        bluez_monitor.properties = {
-          ["bluez5.enable-sbc-xq"] = true,
-          ["bluez5.enable-msbc"] = true,
-          ["bluez5.enable-hw-volume"] = true,
-          ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-        }
-      '';
     };
   };
 
