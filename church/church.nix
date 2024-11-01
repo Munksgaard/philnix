80baf5ec10d1dd77fbb810adc96c5e7b5e38120e
Philip Munksgaard <philip@munksgaard.me>
2024-11-01 10:56:00 +0100

    'nixFlakes' has been renamed to/replaced by 'nixVersions.stable'

--- church/church.nix
+++ church/church.nix
@@ -9,7 +9,7 @@ in {
   imports = [ ./hardware-configuration.nix ];
 
   nix = {
-    package = pkgs.nixFlakes;
+    package = pkgs.nixVersions.stable;
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
--- photos/photos.nix
+++ photos/photos.nix
@@ -16,7 +16,7 @@
   boot.loader.grub.useOSProber = true;
 
   nix = {
-    package = pkgs.nixFlakes;
+    package = pkgs.nixVersions.stable;
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
