954f553a1628679d65c3e4063912d2edbc16c819
Philip Munksgaard <philip@munksgaard.me>
2025-03-12 10:23:03 +0100

    firefox fix

--- church/firefox.nix
+++ church/firefox.nix
@@ -9,7 +9,7 @@
         id = 0;
         name = "default";
         isDefault = true;
-        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+        extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
           ublock-origin
           bitwarden
           consent-o-matic
